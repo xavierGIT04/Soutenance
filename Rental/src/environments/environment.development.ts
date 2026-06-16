d87a4be5035ca252771a46b366de43c566045cfd
xavier <francoiswakegbandi@gmail.com>
2026-06-16 14:42:58 +0000

    modification de angulajson2

--- Rental/src/environments/environment.development.ts
+++ Rental/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   ApiUrl : "http://localhost:8080/api",
   supabaseUrl : 'https://oxlllchsivgvxgdxckpi.supabase.co',
-  supabaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94bGxsY2hzaXZndnhnZHhja3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODQ3NjUsImV4cCI6MjA5NTk2MDc2NX0.U8T4ihoy9sOXVq4ePBKyAwWlCDDctiK50JKzNtT3Cew'
-
+  supabaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94bGxsY2hzaXZndnhnZHhja3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODQ3NjUsImV4cCI6MjA5NTk2MDc2NX0.U8T4ihoy9sOXVq4ePBKyAwWlCDDctiK50JKzNtT3Cew',
+  production:false
 };
