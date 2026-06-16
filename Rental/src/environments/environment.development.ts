6b109ba1be7c51581c071be1f0627f67290c80f3
xavier <francoiswakegbandi@gmail.com>
2026-06-16 16:14:27 +0000

    modification du fichier de prod3

--- Rental/src/environments/environment.development.ts
+++ Rental/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  ApiUrl : "https://rental-api-ubpk.onrender.com",
+  ApiUrl : "https://rental-api-ubpk.onrender.com/api",
   supabaseUrl : 'https://oxlllchsivgvxgdxckpi.supabase.co',
   supabaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94bGxsY2hzaXZndnhnZHhja3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODQ3NjUsImV4cCI6MjA5NTk2MDc2NX0.U8T4ihoy9sOXVq4ePBKyAwWlCDDctiK50JKzNtT3Cew',
   production:false
--- Rental/src/environments/environment.ts
+++ Rental/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ApiUrl: 'https://rental-api-ubpk.onrender.com', // L'URL fournie par Render pour votre Spring Boot
+  ApiUrl: 'https://rental-api-ubpk.onrender.com/api', // L'URL fournie par Render pour votre Spring Boot
   supabaseUrl : 'https://oxlllchsivgvxgdxckpi.supabase.co',
   supabaseKey : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94bGxsY2hzaXZndnhnZHhja3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzODQ3NjUsImV4cCI6MjA5NTk2MDc2NX0.U8T4ihoy9sOXVq4ePBKyAwWlCDDctiK50JKzNtT3Cew'
 };
